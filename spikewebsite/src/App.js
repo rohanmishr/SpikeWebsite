1e5568c5a3af1c40fd519cf37384e76e1de888e2
PillageDev <63556914+PillageDev@users.noreply.github.com>
2023-10-24 14:45:11 -0400

    Initial commit
    Co-authored-by: rohanmishra <rohanmishr@users.noreply.github.com>

---
+++ spikewebsite/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './styles/App.css';
+import Header from './Header';
+import Body from './Body';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ spikewebsite/src/Body.js
@@ -0,0 +1,28 @@
+
+
+function Body() {
+    return (
+        <div className="Body">
+            <div className="FeaturedPost">
+                <div className="FeaturedPostText">
+                    <h1>First Post!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+                </div>
+            </div>
+            <div className="OtherPosts">
+                <h3>Post Title</h3>
+            </div>
+            <div className="OtherPosts">
+                <h3>Post Title</h3>
+            </div>
+            <div className="OtherPosts">
+                <h3>Post Title</h3>
+            </div>
+            <div className="OtherPosts">
+                <h3>Post Title</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
